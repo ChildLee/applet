74d054cdb739591c06d348d532cde09e6e942c2a
ChildLee <471227142@qq.com>
2018-03-08 12:00:38 +0800

    commit

---
+++ src/main/java/com/applet/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.applet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/a")
+    public String a() {
+        return "a";
+    }
+}
