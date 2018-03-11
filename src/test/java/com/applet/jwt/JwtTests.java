a4fe6b5b5d182d3f14aa6f0d186ed087f606011b
ChildLee <471227142@qq.com>
2018-03-11 20:59:04 +0800

    commit

--- src/test/java/com/applet/jwt/JwtTests.java
+++ src/test/java/com/applet/jwt/JwtTests.java
@@ -3,12 +3,14 @@ package com.applet.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
 public class JwtTests {
     /**
@@ -20,7 +22,7 @@ public class JwtTests {
                 .withClaim("name", "zwz")
                 .withClaim("age", "18")
                 .withIssuedAt(new Date())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000))
                 .sign(Algorithm.HMAC256("secret"));
         System.out.println(token);
     }
@@ -30,10 +32,8 @@ public class JwtTests {
      */
     @Test
     public void verifyToken() throws UnsupportedEncodingException {
-        JWTVerifier verifier = null;
-        DecodedJWT jwt = null;
-        verifier = JWT.require(Algorithm.HMAC256("secret")).build();
-        jwt = verifier.verify("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiend6IiwiZXhwIjoxNTIxMDkxODk3LCJpYXQiOjE1MjA0ODcwOTcsImFnZSI6IjE4In0.4i3JjMZTcWVOalQd_Gbe7uj6cCQYmNQWg2N4OcUezXM");
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256("secret")).build();
+        DecodedJWT jwt = verifier.verify("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiend6IiwiZXhwIjoxNTIwNzczMDU1LCJpYXQiOjE1MjA3NzMwNTQsImFnZSI6IjE4In0._Tx87avy0-3fejYMf0-oBelaa1o7KdoX4TqVgo1SyVA");
 
         System.out.println(jwt.getClaims().get("name").asString());
         System.out.println(jwt.getClaim("age").asString());
@@ -47,5 +47,10 @@ public class JwtTests {
      */
     @Test
     public void decodeToken() {
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiend6IiwiZXhwIjoxNTIwNzczMDU1LCJpYXQiOjE1MjA3NzMwNTQsImFnZSI6IjE4In0._Tx87avy0-3fejYMf0-oBelaa1o7KdoX4TqVgo1SyVA";
+        DecodedJWT jwt = JWT.decode(token);
+        Map<String, Claim> claims = jwt.getClaims();
+        Claim claim = claims.get("name");
+        System.out.println(claim.asString());
     }
 }
